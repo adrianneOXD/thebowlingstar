bae0b2707c735bbce6be351ab7de6029f9166036
Jessica <jessica@rockmoongames.io>
2023-05-03 13:44:11 -0700

    Add a first test

---
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package io.rockmoongames.bowlingstar;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+
+class BowlingGameTest{
+
+    @Test
+    public void jessicaTestOne(){
+        
+    }
+}
